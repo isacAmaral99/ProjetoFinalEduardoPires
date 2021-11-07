fcaa217a629d9cf6c20387edc6eb12c0f2aa00c1
isacAmaral99 <isacamaralbueno@gmail.com>
2021-11-07 20:25:37 -0300

    componentes basicos da aplicacao

---
+++ src/app/Navegacao/navegacao.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {RouterModule} from "@angular/router";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MenuComponent } from "./menu/menu.component";
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NotFoundComponent } from "./notfound/notfound.component";
+
+
+@NgModule({
+  declarations:[
+     MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent
+  ],
+  imports:[
+    CommonModule,
+    RouterModule,
+    NgbModule
+
+  ],
+  exports :[
+    MenuComponent,
+        HomeComponent,
+        FooterComponent,
+        NotFoundComponent
+  ]
+})
+export class NavegacaoModule{}
