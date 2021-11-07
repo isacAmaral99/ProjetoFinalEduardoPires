8881e1daeb9645d8c43cbe002e434806674a0df8
isacAmaral99 <isacamaralbueno@gmail.com>
2021-11-07 20:24:45 -0300

    criando aplicacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacaoModule } from './Navegacao/navegacao.module';
+import { MenuComponent } from './Navegacao/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NavegacaoModule,
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,MenuComponent]
 })
 export class AppModule { }
