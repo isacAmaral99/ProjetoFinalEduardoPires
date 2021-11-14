883d9ac6c1f32aedd1f66fb1fbcdc0129cf0c3ad
isacAmaral99 <isacamaralbueno@gmail.com>
2021-11-14 12:00:42 -0300

    implementacao do local storange

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,8 +1,11 @@
 import { HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { throwError } from "rxjs";
+import { LocalStorageUtils } from "../utils/localstorage";
 
 export abstract class BaseService{
 
+
+  public LocalStorage = new LocalStorageUtils();
 
    protected ObterHeaderJson(){
 
